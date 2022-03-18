74ef0a204609f939fe74b88328a3172be70af990
Anass Baba <anasss.contact@gmail.com>
2022-03-19 00:46:09 +0100

    add Model Table User with (ORM,Getters,attribute)

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -12,15 +13,26 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    const PRIMARY_KEY_COLUMN_NAME                   =   'id';
+    const NAME_COLUMN_NAME                          =   'name';
+    const EMAIL_COLUMN_NAME                         =   'email';
+    const GIT_USERNAME_COLUMN_NAME                  =   'gitUsername';
+    const GIT_COUNT_RP_VIEWS_COLUMN_NAME            =   'gitCountRpViews';
+    const GIT_COUNT_RPS_COLUMN_NAME                 =   'gitCountRps';
+    const PASSWORD_COLUMN_NAME                      =   'password';
+    const REMEMBER_TOKEN_COLUMN_NAME                =   'remember_token';
+    const EMAIL_VERIFIED_AT_COLUMN_NAME             =   'email_verified_at';
+    const CREATED_AT__COLUMN_NAME                   =   'created_at';
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        self::NAME_COLUMN_NAME,
+        self::EMAIL_COLUMN_NAME,
+        self::PASSWORD_COLUMN_NAME,
     ];
 
     /**
@@ -29,8 +41,8 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $hidden = [
-        'password',
-        'remember_token',
+        self::PASSWORD_COLUMN_NAME,
+        self::REMEMBER_TOKEN_COLUMN_NAME,
     ];
 
     /**
@@ -39,6 +51,57 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
+        self::EMAIL_VERIFIED_AT_COLUMN_NAME => 'datetime',
     ];
+
+    /**
+     * Getters
+     */
+
+    public function getUserKey()
+    {
+        return $this->getAttributeValue(self::PRIMARY_KEY_COLUMN_NAME);
+    }
+
+    public function getUserName()
+    {
+        return $this->getAttributeValue(self::NAME_COLUMN_NAME);
+    }
+
+    public function getUserEmail()
+    {
+        return $this->getAttributeValue(self::EMAIL_COLUMN_NAME);
+    }
+
+    public function getGitUserName()
+    {
+        return $this->getAttributeValue(self::GIT_USERNAME_COLUMN_NAME);
+    }
+
+    public function getGitCountRpViews()
+    {
+        return $this->getAttributeValue(self::GIT_COUNT_RP_VIEWS_COLUMN_NAME);
+    }
+
+    public function getGitCountRps()
+    {
+        return $this->getAttributeValue(self::GIT_COUNT_RPS_COLUMN_NAME);
+    }
+
+
+
+    /**
+     * ORM relations
+     */
+
+    /**
+     * Table User Has Many Repositories GitHub #UserReps
+     * @return HasMany
+     */
+    public function userReps(): HasMany
+    {
+        return $this->hasMany(UserReps::class,
+            UserReps::USER_KEY_COLUMN_NAME,
+            self::PRIMARY_KEY_COLUMN_NAME);
+    }
 }
