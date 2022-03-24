11105999ec298566a553c5b2fe786a4794c58476
MrAnassBaba <anass.baba.contact@gmail.com>
2022-03-24 14:50:56 +0100

    check value of key app on CI/CD

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Config;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -15,6 +17,8 @@ class ExampleTest extends TestCase
     public function test_example()
     {
         $response = $this->get('/');
+        $value = Config::get('app.key');
+        dd("check value of key app on CI/CD",$value);
 
         $response->assertStatus(200);
     }
